ab074e07a8e2aa6b640081705c6c909aa56889ff
abdi-gitter <152942085+abdi-gitter@users.noreply.github.com>
2024-03-19 21:21:13 -0500

    Add files via upload

---
+++ sass-tour/sass-tour/src/App.js
@@ -0,0 +1,19 @@
+import Main from './components/main/Main'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      {/* make a component to consume array of places,
+       and render another component for each place. */}
+       {/* Main/Card */}
+       <Nav />
+       <Header />
+       <Main />
+    </div>
+  );
+}
+
+export default App;
